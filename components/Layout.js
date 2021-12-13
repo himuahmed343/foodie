28247d2545f3dc24f801c354a9f207e7fd125d47
Himu Ahmed <64636115+himuahmed343@users.noreply.github.com>
2021-12-14 02:15:15 +0600

    Navbar added

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
